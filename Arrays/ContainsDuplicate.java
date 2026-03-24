b9eb3c6ae2ca302df9d258c329aa453ef91d5bd5
ananya-saxena75 <ananyaworks75@gmail.com>
2026-03-24 23:18:24 +0530

    Implement method to check for duplicate elements

---
+++ Arrays/ContainsDuplicate.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+       Set<Integer> set = new HashSet<>();
+       for (int num : nums) set.add(num);
+       if(nums.length != set.size())
+       return true;
+       else 
+       return false;
+    }
+}
